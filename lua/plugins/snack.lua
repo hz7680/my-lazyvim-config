1bf1632822e925a780c8f251a816fbf38bbaeeff
Albert Hua <346187456@qq.com>
2025-04-10 09:06:34 +0800

    add a custom style for float terminal

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -1,9 +1,21 @@
+local styles = require('snacks').config.styles
+
+styles.borderFloatTerminal = {
+  relative = "editor",
+  position = "float",
+  backdrop = 60,
+  height = 0.9,
+  width = 0.9,
+  zindex = 50,
+  border = "rounded"
+}
+
 return {
   "folke/snacks.nvim",
   opts = {
     terminal = {
       win = {
-        style = "float"
+        style = "borderFloatTerminal"
       }
     }
   }
